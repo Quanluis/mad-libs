3d7cfff8b4952094868fe24c6eb8500d2b22e865
Luis Garzon <luisgarzon90@yahoo.com>
2018-02-05 16:34:26 -0500

    Finished mad libs example with js

---
+++ app.js
@@ -0,0 +1,9 @@
+const noun = prompt("Enter a Noun");
+const verb = prompt("Enter a Verb");
+const adj = prompt("Enter a adjective");
+
+alert("You are finished!!");
+
+document.write(noun + " was walking in the forest" + " he " + verb + " the " + adj + " cow" + " and afterwords " + noun + " went home.");
+
+
